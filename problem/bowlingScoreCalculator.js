d37118db008c53e2deec7e6ad47afc21672a5611
Ashutosh Senapati <ashutosh_senapati@mckinsey.com>
2023-01-18 12:44:03 +0530

    feat: frame score calculation

--- problem/bowlingScoreCalculator.js
+++ problem/bowlingScoreCalculator.js
@@ -1,7 +1,7 @@
 function calculateScore(rolls) {
   let frames = [];
-  let n = rolls.length;
-  for (let i = 0; i < n; ) {
+
+  for (let i = 0; i < rolls.length; ) {
     //frame in case of open frame or spare
     if (rolls[i] + rolls[i + 1] <= 10) {
       frames.push([rolls[i], rolls[i + 1]]);
@@ -13,7 +13,33 @@ function calculateScore(rolls) {
       i++;
     }
   }
-  return frames;
+  let totalScore = 0;
+  let frameScores = [];
+  let numberOfFrames = frames.length;
+  while (numberOfFrames--) {
+    let frameScore = 0;
+    frames[numberOfFrames].forEach((element) => {
+      frameScore += element;
+    });
+
+    //open
+    if (frameScore < 10) frameScores.unshift(frameScore);
+    //spare
+    else if (frameScore == 10 && frames[numberOfFrames].length == 2) {
+      frameScore = 10 + frameScores[numberOfFrames + 1];
+      frameScores.unshift(frameScore);
+    }
+    //strike
+    else {
+      frameScore = 10 + frameScores[numberOfFrames + 1];
+      frameScores.unshift(frameScore);
+    }
+  }
+  frameScores.forEach((element) => {
+    totalScore += element;
+  });
+  console.log(frameScores);
+  return totalScore;
 }
 console.log(
   calculateScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
