45b22c501d2587323e401ddf12f268a2678264d0
Ashutosh Senapati <ashutosh_senapati@mckinsey.com>
2023-01-18 16:12:23 +0530

    chore: restructure code

--- problem/bowlingScoreCalculator.js
+++ problem/bowlingScoreCalculator.js
@@ -1,27 +1,35 @@
-function calculateScore(rolls) {
-  let frames = [];
-
+function constructFrames(rolls, frames) {
   for (let i = 0; i < rolls.length; ) {
     //10th frame
     if (frames.length == 9) {
-      if (rolls[i] + rolls[i + 1] >= 10)
+      // consecutive strike or spare
+      if (rolls[i] + rolls[i + 1] >= 10) {
         frames.push([rolls[i], rolls[i + 1], rolls[i + 2]]);
-      else if (rolls[i] == 10) frames.push([rolls[i]]);
-      else frames.push([rolls[i - 1], rolls[i]]);
+        i += 2;
+      } else {
+        frames.push([rolls[i - 1], rolls[i]]);
+        i += 1;
+      }
+      if (i != rolls.length - 1)
+        throw new Error("Number of frames exceeds limit");
+
       break;
     }
+
     //frame in case of open frame or spare
     if (rolls[i] + rolls[i + 1] <= 10) {
       frames.push([rolls[i], rolls[i + 1]]);
       i += 2;
     }
+
     //frame in case of strike
     else {
       frames.push([10]);
       i++;
     }
   }
-  let totalScore = 0;
+}
+function calculateScoresForFrames(frames) {
   let frameScores = [];
   let numberOfFrames = frames.length;
   while (numberOfFrames--) {
@@ -40,17 +48,25 @@ function calculateScore(rolls) {
       frameScore = frameScore + frameScores[numberOfFrames];
       frameScores.unshift(frameScore);
     }
+
     //strike
     else if (frameScore == 10) {
       frameScore = 10 + frameScores[numberOfFrames];
       frameScores.unshift(frameScore);
     }
   }
+  return frameScores;
+}
+function calculateScore(rolls) {
+  let frames = [];
+
+  constructFrames(rolls, frames);
+  let totalScore = 0;
+  let frameScores = calculateScoresForFrames(frames);
   frameScores.forEach((element) => {
     totalScore += element;
   });
-  console.log(frames);
+
   return totalScore;
 }
-
 module.exports = { calculateScore };
